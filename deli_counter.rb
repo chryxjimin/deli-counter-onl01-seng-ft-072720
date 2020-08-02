5bb1b3f7f18b597004242fd7c9ad82808a1cbed5
Christie Yoon <cleex1999@gmail.com>
2020-08-02 22:56:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+#require "pry"
+katz_deli = []
+
+def line(array)
+  if array.size >= 1
+    other_deli = []
+    counter = 1
+    array.each do |name|
+      other_deli.push("#{counter}. #{name}")
+      counter += 1
+  end
+    puts "The line is currently: #{other_deli.join(" ")}"
+ else
+   puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+      puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+      #binding.pry
+end
+
+def now_serving(katz_deli)
+  #katz_deli = []
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  elsif katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  else
+    puts "Currently serving #{katz_deli[0]}."
+   end
+end
